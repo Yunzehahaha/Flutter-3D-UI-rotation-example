89676639390e2237e50fe7dc1da27af2ececd8d3
YunzeOppa <powerking52111@gmail.com>
2021-05-24 22:20:21 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  int _counter = 0;
+  double maxSlide;
+  AnimationController animationController;
+  bool canDrag;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+  }
+
+  void toggle() => animationController.isDismissed
+      ? animationController.forward()
+      : animationController.reverse();
+
+  void _onDragStart(DragStartDetails details) {
+    // bool fromLeft =
+    //     animationController.isDismissed && details.globalPosition.dx < 100;
+    // bool fromRight =
+    //     animationController.isCompleted && details.globalPosition.dx > 300;
+    canDrag = true;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (canDrag) {
+      double delta = details.primaryDelta / maxSlide;
+      animationController.value += delta;
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    if (animationController.isDismissed || animationController.isCompleted) {
+      return;
+    }
+    if (details.velocity.pixelsPerSecond.dx.abs() >= 365.0) {
+      double visualVelocity = details.velocity.pixelsPerSecond.dx /
+          MediaQuery.of(context).size.width;
+      animationController.fling(velocity: visualVelocity);
+    } else if (animationController.value < 0.5) {
+      animationController.reverse();
+    } else {
+      animationController.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var myDrawer = Container(color: Colors.blue[700]);
+    var myMainPage = Container(color: Colors.yellow[300]);
+    maxSlide = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onHorizontalDragStart: _onDragStart,
+          onHorizontalDragUpdate: _onDragUpdate,
+          onHorizontalDragEnd: _onDragEnd,
+          child: AnimatedBuilder(
+            animation: animationController,
+            builder: (BuildContext context, Widget child) {
+              double slide = maxSlide * animationController.value;
+              double scale = 1 - (animationController.value * 0.4);
+              return Stack(
+                children: [
+                  Transform.translate(
+                    offset:
+                        Offset(maxSlide * (animationController.value - 1), 0),
+                    child: Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001)
+                        ..rotateY(
+                            math.pi / 2 * (1 - animationController.value)),
+                      alignment: Alignment.centerRight,
+                      child: myDrawer,
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset(maxSlide * animationController.value, 0),
+                    child: Transform(
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001)
+                          ..rotateY(-math.pi / 2 * animationController.value),
+                        alignment: Alignment.centerLeft,
+                        child: myMainPage),
+                  )
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggle,
+        tooltip: 'Increment',
+        child: Icon(Icons.ac_unit),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
